db9a0609c4095eaff25a0a6ff3cfc70997e0f2bb
Alyona Vyshnevska <alyona.vyshnevska@gmail.com>
2020-02-05 19:32:55 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,5 @@ setup(
     packages=find_packages(),
     version='0.1.0',
     description='Final Project for the Coreference Resolution Course at the University of Potsdam',
-    author='Olena Vyshenvska, Patrick Kahardipraja',
-    license='',
+    author='Olena Vyshnevska, Patrick Kahardipraja',
 )

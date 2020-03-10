e96d5ec7e32cc682e669a01aa9c9e68805019aaf
Patrick Kahardipraja <patrick.khdipraja@gmail.com>
2020-03-10 22:33:25 +0100

    added logging and f1 score

--- src/train_probe.py
+++ src/train_probe.py
@@ -1,26 +1,54 @@
 import h5py
 import sys
 import tensorflow as tf
+import numpy as np
+import argparse
 
 from keras.models import Sequential
 from keras.layers.core import Dense, Activation
 from keras import optimizers
 from keras import backend as K
-from keras.callbacks import EarlyStopping
+from keras.callbacks import EarlyStopping, CSVLogger, Callback
+from sklearn.metrics import f1_score
+
+
+class ComputeTestF1(Callback):
+    """Custom callback to calculate F1 score"""
+    def on_epoch_end(self, epochs, logs):
+        val_predict = (np.asarray(self.model.predict(self.validation_data[0]))).round()
+        val_target = self.validation_data[1]
+        logs['val_f1'] = f1_score(val_target, val_predict)
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Run probing experiment for c2f-coref with BERT embeddings')
+    parser.add_argument('--train_data', type=str, default=None)
+    parser.add_argument('--val_data', type=str, default=None)
+    parser.add_argument('--test_data', type=str, default=None)
+    parser.add_argument('--exp_name', type=str, default=None)
+    args = parser.parse_args()
+    return args
+
 
 if __name__ == "__main__":
-    train_filename = sys.argv[1]
-    val_filename = sys.argv[2]
-    with h5py.File(train_filename, 'r') as f:
+    args = get_args()
+    log_name = args.exp_name + '.log'
+
+    with h5py.File(args.train_data, 'r') as f:
         train_data = f.get('span_representations').value
+        x_train = train_data[:, :-2]
+        y_train = train_data[:, -1].astype(int)
 
-    with h5py.File(val_filename, 'r') as f:
+    with h5py.File(args.val_data, 'r') as f:
         val_data = f.get('span_representations').value
+        x_val = val_data[:, :-2]
+        y_val = val_data[:, -1].astype(int)
 
-    x_train = train_data[:,:-2]
-    y_train = train_data[:,-1].astype(int)
-    x_val = val_data[:,:-2]
-    y_val = val_data[:,-1].astype(int)
+    if args.test_data is not None:
+        with h5py.File(args.test_data, 'r') as f:
+            test_data = f.get('span_representations').value
+            x_test = test_data[:, :-2]
+            y_test = test_data[:, -1].astype(int)
 
     # Probing model implementation using keras, following hyperparameters described in Liu's paper, can finetune later.
     model = Sequential()
@@ -28,10 +56,13 @@ if __name__ == "__main__":
     model.add(Dense(units=1, activation='sigmoid'))
     opt = optimizers.Adam(lr=0.001)
 
-    callbacks = [EarlyStopping(monitor='val_loss', min_delta=0, patience=3, verbose=0, mode='auto')]
+    callbacks = [EarlyStopping(monitor='val_loss', min_delta=0, patience=3, verbose=0, mode='auto'), ComputeTestF1(), CSVLogger(log_name, separator='\t')]
 
     # Compile model
     model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
     model.fit(x_train, y_train, epochs=50, batch_size=10, validation_data=(x_val, y_val), callbacks=callbacks)
-    loss_and_metrics = model.evaluate(x_val, y_val, batch_size=x_val.shape[0])
-    print(loss_and_metrics)
+    val_loss_and_metrics = model.evaluate(x_val, y_val, batch_size=x_val.shape[0])
+    print(val_loss_and_metrics)
+    if args.test_data is not None:
+        test_loss_and_metrics = model.evaluate(x_test, y_test, batch_size=x_test.shape[0])
+        print(test_loss_and_metrics)

49dbcf4d21bc44726bbf05903d75117426d405f2
Patrick Kahardipraja <patrick.khdipraja@gmail.com>
2020-03-21 17:54:51 +0100

    added best checkpoint save

--- src/train_probe.py
+++ src/train_probe.py
@@ -10,7 +10,7 @@ from keras.models import Sequential
 from keras.layers.core import Dense, Activation
 from keras import optimizers
 from keras import backend as K
-from keras.callbacks import EarlyStopping, CSVLogger, Callback
+from keras.callbacks import EarlyStopping, CSVLogger, Callback, ModelCheckpoint
 from sklearn.metrics import f1_score
 
 
@@ -63,7 +63,9 @@ if __name__ == "__main__":
     model.add(Dense(units=1, activation='sigmoid'))
     opt = optimizers.Adam(lr=0.001)
 
-    callbacks = [EarlyStopping(monitor='val_loss', min_delta=0, patience=3, verbose=0, mode='auto', restore_best_weights=True), ComputeTestF1(), CSVLogger(log_name, separator='\t')]
+    early_stop = EarlyStopping(monitor='val_loss', min_delta=0, patience=3, verbose=0, mode='auto', restore_best_weights=True)
+    checkpoint_save = ModelCheckpoint(args.exp_name + '.h5', save_best_only=True, monitor='val_loss', mode='min')
+    callbacks = [early_stop, checkpoint_save, ComputeTestF1(), CSVLogger(log_name, separator='\t')]
 
     # Compile model
     model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])

5a342a5484b1a21e9c684dba8e0d290772c4f29a
Alyona Vyshnevska <alyona.vyshnevska@gmail.com>
2020-04-10 15:47:44 +0200

    add script that outputs 100 first errors with snippets of sentences they occur in for joshi

--- pred/pred_analyse.py
+++ pred/pred_analyse.py
@@ -26,22 +26,39 @@ def get_error_count(input_filename):
                 pred_label = example['pred'][idx]
                 # calculate number of all mention pairs
                 num_examples += 1
+                # if num_examples < 30:
 
-                # calculate true positives...
-                if label == 1 and pred_label == 1:
-                    true_pos += 1
-                elif label == 0 and pred_label == 0:
-                    true_neg +=1
-                elif label == 1 and pred_label == 0:
-                    false_neg +=1
-                elif label == 0 and pred_label == 1:
-                    false_pos += 1
+                #     print(len(example['sentences'][0]))
+                #     print(example['sentences'])
 
-                # calculate number of errors
-                if label != pred_label:
-                    error_counter += 1
+
+                # # calculate true positives...
+                # if label == 1 and pred_label == 1:
+                #     true_pos += 1
+                # elif label == 0 and pred_label == 0:
+                #     true_neg +=1
+                # elif label == 1 and pred_label == 0:
+                #     false_neg +=1
+                # elif label == 0 and pred_label == 1:
+                #     false_pos += 1
+                #
+                # # calculate number of errors
+                # if label != pred_label:
+                #     error_counter += 1
 
                 # print errors
+                if label != pred_label:
+                    error_counter += 1
+                    if error_counter < 100:
+                        sentences = [item for sublist in example['sentences'] for item in sublist]
+                        print(example['mention_dist'][idx])
+                        before = max(0, example['men1_start'][idx] - 5)
+                        after = min(len(sentences)-1, example['men2_end'][idx])
+                        print(sentences[example['men1_start'][idx]:example['men1_end'][idx]+1], ' corefers with ', sentences[example['men2_start'][idx]:example['men2_end'][idx]+1])
+                        print(' in ', sentences[before:after], '\n' )
+
+
+
 
 
     return error_counter, num_examples, true_pos, true_neg, false_pos, false_neg
@@ -54,6 +71,6 @@ if __name__ == '__main__':
     for input_filename in filenames:
         print(input_filename)
         error_count, num_examples, true_pos, true_neg, false_pos, false_neg  = get_error_count(input_filename)
-        print(f'error_count: {error_count} errors in {num_examples} samples. {100 - ((error_count/num_examples)*100):.3f} accuracy % \n')
-        print(f'true_pos, true_neg, false_pos, false_neg, {true_pos, true_neg, false_pos, false_neg}')
+        # print(f'error_count: {error_count} errors in {num_examples} samples. {100 - ((error_count/num_examples)*100):.3f} accuracy % \n')
+        # print(f'true_pos, true_neg, false_pos, false_neg, {true_pos, true_neg, false_pos, false_neg}')
         # print(f'error_clusters: {error_clusters} errors in {num_clusters} clusters. {100 - ((error_clusters/num_clusters)*100):.3f} accuracy % \n')

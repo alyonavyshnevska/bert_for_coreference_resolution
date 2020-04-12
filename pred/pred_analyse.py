9478b10935201ca5f509865b2f68c872cdce1b54
Alyona Vyshnevska <alyona.vyshnevska@gmail.com>
2020-04-12 10:11:07 +0200

    updated version of error analysis output

--- pred/pred_analyse.py
+++ pred/pred_analyse.py
@@ -1,12 +1,12 @@
 import json
-import ast
+
 
 def get_error_count(input_filename):
     '''
     :param input_filename: predicted jsonlines filepath
-    :return:
+    :return: error_counter, num_examples, num of true_pos, true_neg, false_pos, false_neg
 
-    Jsonlines one line:
+    one line of input file:
     dict_keys(['mention_dist', 'men1_end', 'sentences', 'men1_start', 'pred', 'men2_start', 'men2_end', 'gold_label', 'doc_key'])
     '''
     error_counter = 0
@@ -15,7 +15,6 @@ def get_error_count(input_filename):
     true_neg = 0
     false_pos = 0
     false_neg = 0
-    errors = 'errors' + input_filename
 
     # with open(errors, 'w'):
     with open(input_filename) as input_file:
@@ -26,41 +25,32 @@ def get_error_count(input_filename):
                 pred_label = example['pred'][idx]
                 # calculate number of all mention pairs
                 num_examples += 1
-                # if num_examples < 30:
 
-                #     print(len(example['sentences'][0]))
-                #     print(example['sentences'])
+                # only a subset of example for error analysis
+                if num_examples < 750:
 
+                    # calculate true positives, false positives...
+                    if label == 1 and pred_label == 1:
+                        true_pos += 1
+                    elif label == 0 and pred_label == 0:
+                        true_neg +=1
+                    elif label == 1 and pred_label == 0:
+                        false_neg +=1
+                    elif label == 0 and pred_label == 1:
+                        false_pos += 1
 
-                # # calculate true positives...
-                # if label == 1 and pred_label == 1:
-                #     true_pos += 1
-                # elif label == 0 and pred_label == 0:
-                #     true_neg +=1
-                # elif label == 1 and pred_label == 0:
-                #     false_neg +=1
-                # elif label == 0 and pred_label == 1:
-                #     false_pos += 1
-                #
-                # # calculate number of errors
-                # if label != pred_label:
-                #     error_counter += 1
-
-                # print errors
-                if label != pred_label:
-                    error_counter += 1
-                    if error_counter < 100:
+                    # output errors
+                    if label != pred_label:
+                        error_counter += 1
                         sentences = [item for sublist in example['sentences'] for item in sublist]
-                        print(example['mention_dist'][idx])
-                        before = max(0, example['men1_start'][idx] - 7)
-                        after = min(len(sentences)-1, example['men2_end'][idx])
-                        print(" ".join(sentences[example['men1_start'][idx]:example['men1_end'][idx]+3]), '   ==   ', " ".join(sentences[example['men2_start'][idx]:example['men2_end'][idx]+1]))
-                        if example['mention_dist'][idx] < 20:
-                            print(' in ', " ".join(sentences[before:after]), '\n' )
+                        print(example['mention_dist'][idx], 'pred:', example['pred'][idx], 'gold:', example['gold_label'][idx])
+                        before = max(0, example['men1_start'][idx] - 10)
+                        after = min(len(sentences)-1, example['men2_end'][idx]+3)
+                        print(" ".join(sentences[example['men1_start'][idx]:example['men1_end'][idx]+1]), '   ==   ', " ".join(sentences[example['men2_start'][idx]:example['men2_end'][idx]+1]))
+                        if example['mention_dist'][idx] < 15:
+                            print('...', " ".join(sentences[before:after]), "(", example['mention_dist'][idx], ")", '\n' )
                         else:
-                            print('in', " ".join(sentences[before:example['men1_end'][idx]+5]), '...', " ".join(sentences[example['men2_start'][idx]-5:example['men2_end'][idx]+5]), '\n' )
-
-
+                            print('...', " ".join(sentences[before:example['men1_end'][idx]+3]), '...', " ".join(sentences[example['men2_start'][idx]-10:after]), "(", example['mention_dist'][idx], ")", '\n' )
 
 
 
@@ -69,11 +59,8 @@ def get_error_count(input_filename):
 
 if __name__ == '__main__':
     filenames = ["pred_test.english.128.probe_reduce.joshi.jsonlines", "pred_test.english.384.probe_reduce.joshi.jsonlines"]
-    # input_filename = "pred_test.english.128.probe_reduce.joshi.jsonlines"
 
     for input_filename in filenames:
         print(input_filename)
         error_count, num_examples, true_pos, true_neg, false_pos, false_neg  = get_error_count(input_filename)
-        # print(f'error_count: {error_count} errors in {num_examples} samples. {100 - ((error_count/num_examples)*100):.3f} accuracy % \n')
-        # print(f'true_pos, true_neg, false_pos, false_neg, {true_pos, true_neg, false_pos, false_neg}')
-        # print(f'error_clusters: {error_clusters} errors in {num_clusters} clusters. {100 - ((error_clusters/num_clusters)*100):.3f} accuracy % \n')
+        print(f'true_pos, true_neg, false_pos, false_neg, {true_pos, true_neg, false_pos, false_neg}')
